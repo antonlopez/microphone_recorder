a55170cb09d57e67e43202f83e2c3e53cd7a5b9d
Juan Lopez <anton.lopez09@gmail.com>
2017-07-06 15:06:21 -0400

    audio recorder and player working

--- src/App.js
+++ src/App.js
@@ -3,25 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 import Record from './record';
 import Audio from 'translation-audio-player';
+import LoginControl from './example';
 
 
 
-var file = [];
 
 class App extends Component {
 
 
 
 
+
     render() {
 
-        file[0] = {
-            "name": this.props.name,
-            "src": "https://files.freemusicarchive.org/music%2Fno_curator%2FThe_Womb%2FBang_-_An_Introduction_to_The_Womb%2FThe_Womb_-_02_-_Sex_Club.mp3",
-            //"src": this.props.AudioURL,
 
-            "img": "http://oss.tan8.com/yuepuku/100/50357/50357_prev.jpg?v=1487820191"
-        }
+
+
+
 
 
 
@@ -35,26 +33,6 @@ class App extends Component {
                 <p className="App-intro">
                     To get started, edit <code>src/App.js</code> and save to reload.
                 </p>
-                {console.log("result:", this.props.AudioURL)}
-                <Audio
-                    width={800}
-                    height={100}
-                    fullPlayer={false}
-                    comment={false}
-                    recordButton={() => {
-                        alert('Hello Juan');
-                    }}
-                    color="#212121"
-                    playlist={file}
-                    style={{
-                        boxShadow: '1px 2px 6px rgba(0, 0, 0, 0.2)',
-                        width: '800px',
-                        height: '50px'
-                    }}
-
-                    // store a reference of the audio component
-                    ref={(App) => { this.App = App; }}
-                />
 
 
                 <div><Record/></div>
--- src/record.js
+++ src/record.js
@@ -1,16 +1,23 @@
 import React, {Component} from 'react';
-
 import { ReactMic } from 'react-mic';
+
+
+
 
 export class Record extends Component {
     constructor(props) {
         super(props);
         this.state = {
             record: false,
-            AudioURL: 'https://files.freemusicarchive.org/music%2Fno_curator%2FThe_Womb%2FBang_-_An_Introduction_to_The_Womb%2FThe_Womb_-_02_-_Sex_Club.mp3'
+            displayPlayer: false,
+            AudioURL: ""
         }
 
+
+
         this.onStop = this.onStop.bind(this);
+
+
 
     }
 
@@ -18,27 +25,60 @@ export class Record extends Component {
         this.setState({
             record: true
         });
+
+        this.setState({displayPlayer: false})
+
+
+
+
     }
 
     stopRecording = () => {
         this.setState({
             record: false
         });
+
+
     }
 
     onStop(recordedBlob) {
 
-        console.log('recordedBlob is: ', recordedBlob);
+        this.setState({displayPlayer: true})
 
         this.setState({AudioURL: recordedBlob.blobURL });
-
-        console.log('The audioURL is:', this.state.AudioURL);
 
 
     }
 
+
+
+
+
     render() {
+
+
+        const displayPlayer = this.state.displayPlayer;
+
+        const AudioURL = this.state.AudioURL;
+
+        console.log('AudioURL from render', AudioURL);
+
+        let button = <StopButton onClick={this.stopRecording} />;
+
+        let AudioPlayer = null;
+
+        if (displayPlayer) {
+            AudioPlayer =  <DisplayAudioPlayer displayPlayer={displayPlayer} AudioURL = {AudioURL} />;
+        }
+
+
+
+
         return (
+
+
+
+
             <div>
                 <ReactMic
                     record={this.state.record}
@@ -48,13 +88,69 @@ export class Record extends Component {
                     backgroundColor="#000000" />
                 <div>
                     <button onClick={this.startRecording} type="button">Start</button>
-                    <button onClick={this.stopRecording} type="button">Stop</button>
 
+                    {button}
+
+                 <div>
+                     {AudioPlayer}
+                 </div>
                 </div>
+
+
 
             </div>
         );
     }
+}
+
+
+
+function StopButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Stop
+        </button>
+    );
+}
+
+
+
+
+
+function AudioPlayerComment(props) {
+
+
+    return (
+
+        <video controls name="media">
+            <source src= '' type = "audio/webm" />
+
+        </video>
+
+
+
+    );
+
+
+
+}
+
+function DisplayAudioPlayer(props) {
+    const displayPlayer = props.displayPlayer;
+
+    const AudioURL = props.AudioURL;
+
+
+    if (displayPlayer) {
+
+        return (
+
+               <video controls name="media">
+                  <source src= {AudioURL} type = "audio/webm" />
+               </video>
+        );
+    }
+    return null;
 }
 
 
